628b4ede2e7ed34a9dc624e855f9caf8c63518ec
Aklysson Roberto <aklyroberto@hotmail.com>
2022-09-06 15:35:42 -0300

    starting

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-   
-    </div>
-  );
+const App = () => {
+  return(
+  <div>...</div>
+  )
 }
 
-export default App;
+export default App
