4b3b78580ffa85fdb1874d92a01bcb86bec7f503
Aklysson Roberto <aklyroberto@hotmail.com>
2022-09-06 22:05:06 -0300

    adding styled components

---
+++ src/App.styles.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 100%;
+    max-width: 750px;
+    margin: 0;
+    display: flex;
+    padding: 50px 0;
+`
+
+export const Info = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: auto;
+`
+
+export const LogoLink = styled.a`
+
+`
+
+export const InfoArea = styled.div`
+
+`
+
+export const GridArea = styled.div`
+    flex: 1;
+    display: flex;
+    justify-content: flex-end;
+`
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
+import * as C from './App.styles';
+import Logo from './assets/devmemory_logo.png';
+
 const App = () => {
   return(
-  <div>...</div>
+  <C.Container>
+    <C.Info>
+      <C.LogoLink>
+        <img src={Logo} width='200px' alt='Logo' />;
+      </C.LogoLink>
+      <C.InfoArea>
+        ...
+      </C.InfoArea>
+      <button>Reiniciar</button>
+    </C.Info>
+    <C.GridArea>
+      ...
+    </C.GridArea>
+  </C.Container>
   )
 }
 
